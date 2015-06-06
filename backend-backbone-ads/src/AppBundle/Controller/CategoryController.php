29cad89a79126097e44bad4346c9e6189eae96bf
Bruno Vitorino <bruno.vitorino@gmail.com>
2015-06-06 16:11:14 +0200

    Add entity Ad and Category. Add CRUD for entities.

---
+++ backend-backbone-ads/src/AppBundle/Controller/CategoryController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace AppBundle\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use AppBundle\Entity\Category;
+
+/**
+ * Category controller.
+ *
+ * @Route("/category")
+ */
+class CategoryController extends Controller
+{
+
+    /**
+     * Lists all Category entities.
+     *
+     * @Route("/", name="category")
+     * @Method("GET")
+     * @Template()
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entities = $em->getRepository('AppBundle:Category')->findAll();
+
+        return array(
+            'entities' => $entities,
+        );
+    }
+
+    /**
+     * Finds and displays a Category entity.
+     *
+     * @Route("/{id}", name="category_show")
+     * @Method("GET")
+     * @Template()
+     */
+    public function showAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('AppBundle:Category')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Category entity.');
+        }
+
+        return array(
+            'entity'      => $entity,
+        );
+    }
+}
---
+++ backend-backbone-ads/src/AppBundle/Entity/Ad.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Ad
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="AppBundle\Entity\AdRepository")
+ */
+class Ad
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="title", type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="body", type="string", length=255)
+     */
+    private $body;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="publication_time", type="datetimetz")
+     */
+    private $publicationTime;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     * @return Ad
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string 
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set body
+     *
+     * @param string $body
+     * @return Ad
+     */
+    public function setBody($body)
+    {
+        $this->body = $body;
+
+        return $this;
+    }
+
+    /**
+     * Get body
+     *
+     * @return string 
+     */
+    public function getBody()
+    {
+        return $this->body;
+    }
+
+    /**
+     * Set publicationTime
+     *
+     * @param \DateTime $publicationTime
+     * @return Ad
+     */
+    public function setPublicationTime($publicationTime)
+    {
+        $this->publicationTime = $publicationTime;
+
+        return $this;
+    }
+
+    /**
+     * Get publicationTime
+     *
+     * @return \DateTime 
+     */
+    public function getPublicationTime()
+    {
+        return $this->publicationTime;
+    }
+}
